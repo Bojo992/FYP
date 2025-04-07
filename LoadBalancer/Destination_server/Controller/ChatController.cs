4b66b5f7d94678bdc48eb27073563894dda772a9
Bojo992 <mm22mm44bbi@gmail.com>
2025-04-07 18:06:07 +0100

    fix for not receiving your own connection

--- LoadBalancer/Destination_server/Controller/ChatController.cs
+++ LoadBalancer/Destination_server/Controller/ChatController.cs
@@ -15,8 +15,10 @@ public class ChatController : Hub
             Content = content,
             SentTime = DateTime.UtcNow
         };
+        
         MessageHistory.Add(userMessage);
-        await Clients.Others.SendAsync("ReceiveMessage", senderId, content, userMessage.SentTime);
+        await Clients.Others.SendAsync("ReceiveMessage", senderId, content, userMessage.SentTime, false);
+        await Clients.Caller.SendAsync("ReceiveMessage", senderId, content, userMessage.SentTime, true);
     }
     public async Task RetrieveMessageHistory() => 
         await Clients.Caller.SendAsync("MessageHistory", MessageHistory);
